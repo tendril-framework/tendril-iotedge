01f6574d33245f0276685cf7a2fa1a09a76c1dd0
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2023-04-06 00:20:20 +0530

    Add device configuration api to the iotcore API router

---
+++ src/tendril/apiserver/routers/iotcore.py
@@ -0,0 +1,51 @@
+
+
+from fastapi import APIRouter
+from fastapi import Depends
+
+from tendril.authn.users import auth_spec
+from tendril.authn.users import authn_dependency
+from tendril.authn.users import AuthUserModel
+
+from tendril.config import INTERESTS_API_ENABLED
+from tendril.iotedge.profiles import device_config_unified_model
+
+from tendril.iotcore.settings import get_device_settings
+from tendril.iotcore.settings import set_device_settings
+
+from tendril.utils.db import get_session
+from tendril.utils import log
+logger = log.get_logger(__name__, log.DEFAULT)
+
+
+device_management_router = APIRouter(
+    prefix='/devices', tags=["IoT Device Hub Management API"],
+    dependencies=[Depends(authn_dependency)])
+
+
+@device_management_router.get("/{id}/settings",
+                              response_model=device_config_unified_model)
+async def read_device_settings(id: int,
+                               user: AuthUserModel = auth_spec(scopes=['device:read'])):
+    with get_session() as session:
+        return get_device_settings(id=id, auth_user=user,
+                                   session=session).export()
+
+
+@device_management_router.post("/{id}/settings",
+                               response_model=device_config_unified_model)
+async def write_device_settings(id: int, settings: device_config_unified_model,
+                                user: AuthUserModel = auth_spec(scopes=['device:write'])):
+    with get_session() as session:
+        result = set_device_settings(id=id, settings=settings,
+                                     auth_user=user, session=session)
+        return get_device_settings(id=id, auth_user=user,
+                                   session=session).export()
+
+if INTERESTS_API_ENABLED:
+    routers = [
+        device_management_router
+    ]
+else:
+    logger.info("Not creating Device Management API routers.")
+    routers = []
---
+++ src/tendril/iotcore/settings.py
@@ -0,0 +1,28 @@
+
+
+from tendril.libraries import interests
+from tendril.iotedge import profiles
+from tendril.common.interests.exceptions import InterestTypeUnsupported
+
+from tendril.utils.db import with_db
+from tendril.utils import log
+logger = log.get_logger(__name__, log.DEFAULT)
+
+
+@with_db
+def get_device_settings(id: int, auth_user=None, session=None):
+    library = interests.find_library(id)
+    interest = library.item(id=id, session=session)
+    if not hasattr(interest, 'appname'):
+        raise InterestTypeUnsupported("'appname' attribute", id=id, name=interest.name)
+    profile = interest.profile(auth_user=auth_user, session=session)
+    return profile.config(session=session)
+
+
+@with_db
+def set_device_settings(id: int, settings=None, auth_user=None, session=None):
+    library = interests.find_library(id)
+    interest = library.item(id=id, session=session)
+    if not hasattr(interest, 'appname'):
+        raise InterestTypeUnsupported("'appname' attribute", id=id, name=interest.name)
+    return interest.configure(settings=settings, auth_user=auth_user, session=session)
