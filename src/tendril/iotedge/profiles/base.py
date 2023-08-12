d80d7d63ee86e17cd7147fc56aed9fed4c5c5b49
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2023-08-12 13:26:01 +0530

    Add fallbacks for inconsistent calls.

--- src/tendril/iotedge/profiles/base.py
+++ src/tendril/iotedge/profiles/base.py
@@ -4,6 +4,8 @@ import arrow
 
 from tendril.db.models.deviceconfig import DeviceConfigurationModel
 from tendril.utils.db import with_db
+from tendril.utils import log
+logger = log.get_logger(__name__, log.DEFAULT)
 
 
 class DeviceProfile(object):
@@ -12,11 +14,19 @@ class DeviceProfile(object):
     config_model = DeviceConfigurationModel
 
     def __init__(self, model_instance):
-        self.model_instance = model_instance
+        self._model_instance = model_instance
+
+    @property
+    def model_instance(self):
+        return self._model_instance
 
     def interest(self):
         from tendril import interests
-        return interests.type_codes[self.interest_type](self.model_instance)
+        itype = interests.type_codes[self.interest_type]
+        if isinstance(self.model_instance, itype):
+            return self.model_instance
+        rv = itype(self.model_instance)
+        return rv
 
     def report_seen(self, background_tasks=None):
         self.interest().monitor_report('last_seen', arrow.utcnow(), background_tasks=background_tasks)
@@ -28,11 +38,16 @@ class DeviceProfile(object):
 
     @with_db
     def config(self, session=None):
-        session.add(self.model_instance)
-        cfg = self.model_instance.config
+        if hasattr(self.model_instance, 'model_instance'):
+            logger.warn("model_instance is set to something that isn't a model instance.")
+            model_instance = self.model_instance.model_instance
+        else:
+            model_instance = self.model_instance
+        session.add(model_instance)
+        cfg = model_instance.config
         if not cfg:
             cfg = self.config_model()
-            self.model_instance.config = cfg
+            model_instance.config = cfg
             session.add(cfg)
             session.flush()
         return cfg
