134a47c6a913bb0389689ac1972d549e2a47eb4c
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2024-04-17 00:12:59 +0530

    Create the device aggregation mq

---
+++ src/tendril/core/topology/iotedge.py
@@ -0,0 +1,11 @@
+
+
+from tendril.core.mq.aio import with_mq_client
+from tendril.utils import log
+logger = log.get_logger(__name__)
+
+
+@with_mq_client('X')
+async def create_mq_topology(mq=None):
+    logger.info("Creating the Device Communications queue.")
+    device_aggregation_queue = await mq.create_work_queue('device_aggregation', topic='device.from.*.#')
