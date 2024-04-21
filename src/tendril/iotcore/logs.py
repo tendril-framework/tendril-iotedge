4dd4601bf37d4c5ffe5f4e9ba40f10f5b2f8059e
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2024-04-21 16:04:32 +0530

    Implement get_logs

--- src/tendril/iotcore/logs.py
+++ src/tendril/iotcore/logs.py
@@ -2,6 +2,8 @@
 
 from tendril.libraries import interests
 from tendril.common.interests.exceptions import InterestTypeUnsupported
+from tendril.artefacts.db.controller import get_interest_artefacts
+from tendril.filestore.db.model import StoredFileModel
 
 from tendril.utils.db import with_db
 from tendril.utils import log
@@ -24,5 +26,10 @@ def available_device_logs(id: int, background_tasks=None, auth_user=None, sessio
     interest = library.item(id=id, session=session)
     if not hasattr(interest, 'appname'):
         raise InterestTypeUnsupported("'appname' attribute", id=id, name=interest.name)
-    # TODO find logs in filestore and return links
-    raise NotImplementedError
+
+    log_files = get_interest_artefacts(interest_id=interest.id,
+                                       type=StoredFileModel,
+                                       label="device_logs",
+                                       session=session)
+
+    return log_files
